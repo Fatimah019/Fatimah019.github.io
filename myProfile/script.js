5a60cb8a6398503ab52e3dac211050c490a00a8d
Fatimah <daviesfatimah97@gmail.com>
2020-04-15 20:51:29 -0700

    my profile

---
+++ myProfile/script.js
@@ -0,0 +1,22 @@
+//hire me or contact pop up on click
+document.querySelector("#hire").onclick=()=>{
+    console.log("show hire");
+};
+
+//togle between nav button and nav list
+let navList=document.querySelector("#nav-list");
+let navBtn=document.querySelector("#nav-btn");
+showNavBtn=()=>{
+    if ((window.innerWidth<800) || (navList.style.display==="block")){
+            navList.style.display="none";
+            navBtn.style.display="block";
+    }
+    else{
+        navList.style.display="block";
+        navBtn.style.display="none";
+    }
+}
+showNavBtn();
+
+
+
